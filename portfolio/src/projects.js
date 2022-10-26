48480bc2f562a70194cf3a54e632aef3ded5d6a1
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-10-26 12:59:00 +0000

    update projects

--- portfolio/src/projects.js
+++ portfolio/src/projects.js
@@ -2,20 +2,20 @@ export const projects = [
   {
     imagePath: "./img/react-restaurant.jpg",
     title: "Restaurant Menu",
-    description: "Used React.js to complete this.",
+    description: "API no longer available. Used React JS for this project. Was able to create a card component and re-use it based on the data from API",
     repoLink: "https://github.com/alexGalvan0/react-restaurant",
   },
   {
     imagePath: "./img/tic-tac-toe.jpg",
     title: "Tic-Tac-Toe",
-    description: "Used React.js to complete this.",
+    description: "All elements created dynamically using JS. Used local storage API to keep score after page refresh.",
     repoLink: "https://github.com/alexGalvan0/tic-tac-toe",
     liveDemo: "https://alexgalvan0.github.io/tic-tac-toe/",
   },
   {
     imagePath: "./img/todo.jpg",
     title: "Todo App",
-    description: "Used React.js to complete this.",
+    description: "CRUD application using React JS. This application also uses local storage to remember user's tasks and state of application",
     repoLink: "https://github.com/alexGalvan0/react-todo",
     liveDemo: "https://alexgalvan0.github.io/react-todo/",
   },
@@ -29,7 +29,7 @@ export const projects = [
   {
     imagePath: "./img/weather-app.jpg",
     title: "Weather App",
-    description: "Dynamically used JS to create all HTML elements.",
+    description: "Dynamically used JS to create all HTML elements. Used Axios to call the open weather API",
     repoLink: "https://github.com/alexGalvan0/weather-app",
     liveDemo: "https://alexgalvan0.github.io/weather-app/",
   },
