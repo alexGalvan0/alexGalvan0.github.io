b27e504ac53ade14cf8b0ae7c198e6019f2dabbf
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-10-27 11:18:02 +0000

    blog update

--- portfolio/src/Blogs.js
+++ portfolio/src/Blogs.js
@@ -2,7 +2,8 @@ export const blogs =[
     {
         id:'headingOne',
         date:'August 29th 2022',
-        question1:'Was your first week what you expected? Why? Why not?'
+        question1:'Was your first week what you expected? Why? Why not?',
+        answer1:'Before the bootcamp, I really did not have any expectations,however I was not expecting the amount of reading we did this firstweek. I also did not expect to lear as much about git as I did! Theway that git works is so freaking cool. To know how the industryreleseas beta applications or features then deploys them to be the stable build is pretty great.'
 
-    }
+    },
 ];
--- portfolio/src/components/Blog.js
+++ portfolio/src/components/Blog.js
@@ -1,4 +1,4 @@
-function Blog({id, date,question1}) {
+function Blog({ id, date, question, answer }) {
   return (
     <div className="accordion" id="accordionExample">
       <div className="accordion-item">
@@ -21,90 +21,12 @@ function Blog({id, date,question1}) {
           data-bs-parent="#accordionExample"
         >
           <div className="accordion-body">
-            <strong>
-              {" "}
-              {question1}
-            </strong>
-            <br /> Before the bootcamp, I really did not have any expectations,
-            however I was not expecting the amount of reading we did this first
-            week. I also did not expect to lear as much about git as I did! The
-            way that git works is so freaking cool. To know how the industry
-            releseas beta applications or features then deploys them to be the
-            stable build is pretty great.
+            <strong> {question}</strong>
+            <br /> {answer}
           </div>
           <div className="accordion-body">
-            <strong>
-              {" "}
-              Was your first week what you expected? Why? Why not?
-            </strong>
-            <br /> Before the bootcamp, I really did not have any expectations,
-            however I was not expecting the amount of reading we did this first
-            week. I also did not expect to lear as much about git as I did! The
-            way that git works is so freaking cool. To know how the industry
-            releseas beta applications or features then deploys them to be the
-            stable build is pretty great.
-          </div>
-        </div>
-      </div>
-      <div className="accordion-item">
-        <h2 className="accordion-header" id="headingTwo">
-          <button
-            className="accordion-button collapsed"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseTwo"
-            aria-expanded="false"
-            aria-controls="collapseTwo"
-          >
-            Accordion Item #2
-          </button>
-        </h2>
-        <div
-          id="collapseTwo"
-          className="accordion-collapse collapse"
-          aria-labelledby="headingTwo"
-          data-bs-parent="#accordionExample"
-        >
-          <div className="accordion-body">
-            <strong>This is the second item's accordion body.</strong> It is
-            hidden by default, until the collapse plugin adds the appropriate
-            classNamees that we use to style each element. These classNamees
-            control the overall appearance, as well as the showing and hiding
-            via CSS transitions. You can modify any of this with custom CSS or
-            overriding our default variables. It's also worth noting that just
-            about any HTML can go within the <code>.accordion-body</code>,
-            though the transition does limit overflow.
-          </div>
-        </div>
-      </div>
-      <div className="accordion-item">
-        <h2 className="accordion-header" id="headingThree">
-          <button
-            className="accordion-button collapsed"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseThree"
-            aria-expanded="false"
-            aria-controls="collapseThree"
-          >
-            Accordion Item #3
-          </button>
-        </h2>
-        <div
-          id="collapseThree"
-          className="accordion-collapse collapse"
-          aria-labelledby="headingThree"
-          data-bs-parent="#accordionExample"
-        >
-          <div className="accordion-body">
-            <strong>This is the third item's accordion body.</strong> It is
-            hidden by default, until the collapse plugin adds the appropriate
-            classNamees that we use to style each element. These classNamees
-            control the overall appearance, as well as the showing and hiding
-            via CSS transitions. You can modify any of this with custom CSS or
-            overriding our default variables. It's also worth noting that just
-            about any HTML can go within the <code>.accordion-body</code>,
-            though the transition does limit overflow.
+            <strong>{question}</strong>
+            <br /> {answer}
           </div>
         </div>
       </div>
