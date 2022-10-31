8d19318771c3056975e29994f76073e3417618f1
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-10-31 22:09:29 +0000

    update

--- portfolio/src/Blogs.js
+++ portfolio/src/Blogs.js
@@ -42,30 +42,94 @@ export const blogs = [
     control: "collapseThree",
     week: "Week 3",
     date:"September 11th 2022",
-    answer:
-      "HTML semantics are so fascinating to me. The HTML tag goes beyond just displaying to a page. They are also used by search engines and\
-        other web tools to be able to give the user a better experience. You could take the default styling off from the browser, and no matter \
-        what tag you use, it would all look the same. CSS is an absolute beast. You always think about languages that are difficult to pick up. CSS\
-        should be up there. With all of the different properties, there are some really cool things you can do. Technically, CSS is a style language\
-        but I see functionality support increasing over the past few years. Besides Just changing color and size of HTML elements, you are now able\
-        to calculate, create variables and more to be able to make apps more dynamic. This is needed because of how many different platforms we have now.\
-        including: web, ios, android, mac, windows and more I am sure. All of these platforms have their own different screen sizes and shapes. CSS has adapted\
-        to be able to help the developer create dynamic elements that can detect the platform it is on and adapt. I am trying to figure out how much time I want \
-        to spend on CSS vs Javascripts and other technologies. I mainly care more about App functionality and ease of use, however in my experience working with\
-        graphic designers and just as a consumer in general, It is  very important to make sure something not only looks good, but it's intuitive across all sorts\
-        of platforms. CSS, easy to learn, hard to get good at.  Javascript has been my favorite out of the technologies we have learned so far. I see it\
-        as a playground to be able to create solutions to an opportuniy. With JS, the sky is the limmit. Some people will express creativity in CSS and HTML;\
-        My creativity will be in Javascript. Thinking of logic to solve problems is my happy place. CSS and HTML are the things that people see where as JS\
-        is the 'Magic' that brings it together. It allows applications to interact with the world with APIs. I am so very thankful to have an oportunity to\
-        learn from trained professionals in this field. I am very thankful to be able to have the opportunity to do this bootcamp. I have been trying to\
-        teach myself all of these things on my own for the past 3 or so years. There has always been this internal battle that I am comfortable in this\
-        amazing job, with an amazing team and all of these great benefits, howerver there was always an itch to grow in development. I have been keeping\
-        it a secret because I knew that my current employer was in no need of a developer and didn't want to show my cards just yet for the fear of \
-        getting let go. (weird i know). I have this sense of peace being in this corse, learning what I want to be learning. Pseudocode is like eating\
-        healthy. You don't want to, but it's good for you. The more you do it, the easier it gets. As process oriented as I am, when does the scope of\
-        you project end? In some of the examples from the Hopper group, I saw that some people had logic that IF they didn't have a material needed to\
-        continue in their process, go to the store. Well, what the process of going to the store, and buying something? I know this is an overkill to\
-        think about all of this when you are designing an app, or is it? I enjoy that pseudocode makes you think about all these things, however \
-        I need to scope the project and stick to it. Be comfortable at ending the program without the outcome you are trying to accomplish.",
+    answer: "HTML semantics are so fascinating to me. The HTML tag goes beyond just displaying to a page. They are also used by search engines and\
+            other web tools to be able to give the user a better experience. You could take the default styling off from the browser, and no matter \
+            what tag you use, it would all look the same. CSS is an absolute beast. You always think about languages that are difficult to pick up. CSS\
+            should be up there. With all of the different properties, there are some really cool things you can do. Technically, CSS is a style language\
+            but I see functionality support increasing over the past few years. Besides Just changing color and size of HTML elements, you are now able\
+            to calculate, create variables and more to be able to make apps more dynamic. This is needed because of how many different platforms we have now.\
+            including: web, ios, android, mac, windows and more I am sure. All of these platforms have their own different screen sizes and shapes. CSS has adapted\
+            to be able to help the developer create dynamic elements that can detect the platform it is on and adapt. I am trying to figure out how much time I want \
+            to spend on CSS vs Javascripts and other technologies. I mainly care more about App functionality and ease of use, however in my experience working with\
+            graphic designers and just as a consumer in general, It is  very important to make sure something not only looks good, but it's intuitive across all sorts\
+            of platforms. CSS, easy to learn, hard to get good at.  Javascript has been my favorite out of the technologies we have learned so far. I see it\
+            as a playground to be able to create solutions to an opportuniy. With JS, the sky is the limmit. Some people will express creativity in CSS and HTML;\
+            My creativity will be in Javascript. Thinking of logic to solve problems is my happy place. CSS and HTML are the things that people see where as JS\
+            is the 'Magic' that brings it together. It allows applications to interact with the world with APIs. I am so very thankful to have an oportunity to\
+            learn from trained professionals in this field. I am very thankful to be able to have the opportunity to do this bootcamp. I have been trying to\
+            teach myself all of these things on my own for the past 3 or so years. There has always been this internal battle that I am comfortable in this\
+            amazing job, with an amazing team and all of these great benefits, howerver there was always an itch to grow in development. I have been keeping\
+            it a secret because I knew that my current employer was in no need of a developer and didn't want to show my cards just yet for the fear of \
+            getting let go. (weird i know). I have this sense of peace being in this corse, learning what I want to be learning. Pseudocode is like eating\
+            healthy. You don't want to, but it's good for you. The more you do it, the easier it gets. As process oriented as I am, when does the scope of\
+            you project end? In some of the examples from the Hopper group, I saw that some people had logic that IF they didn't have a material needed to\
+            continue in their process, go to the store. Well, what the process of going to the store, and buying something? I know this is an overkill to\
+            think about all of this when you are designing an app, or is it? I enjoy that pseudocode makes you think about all these things, however \
+            I need to scope the project and stick to it. Be comfortable at ending the program without the outcome you are trying to accomplish.",
   },
+  {
+    id: "headingFour",
+    classSetting: "collapsed", //collapsed
+    control: "collapseFour",
+    week: "Week 4",
+    date: "September 17th 2022",
+    answer:"The daily standup meetings were super helpful. \
+            Reading the documentation was actually super helpfull as \
+            well. I know it is not the most fun to do, but there is some \
+            gold in there. I would find some key words thatI could then later \
+            look up on youtube and get a more visual representation. When it \
+            came to git, I never thought I would be using the cli for it. Now \
+            I am crystal on the concepts that we use. Super helpful for when a \
+            job comes around, I can be confident. Having a 5 month puppy has also \
+            been a challenge becuase of all of the attention it requires. Overall a \
+            plesant experince. I really do not use social media, or are on my phone a \
+            lot. I struggle having checking the slack. During onboarding, I was still \
+            working around 20ish hours a week which I am glad I had the opportunity to \
+            do so however, I had some tough days where I had too much on work and \
+            bootcamp tasks to do. There were days that the only way to complete everything \
+            is waking up around 5am which was tough because I crashed pretty hard the last \
+            demo day of onboarding.My wife set up an office space for me in an un used room.\
+            I have a very comortable setup that allowed me to focus on learning. Becuase \
+            I knew what we were going to be learning during the bootcamp, I have been trying \
+            to teach myself the content for the past few months. This really helped me keep \
+            my head above water I believe. Seeing some progress has also fuled me as well. I \
+            Love getting AHA! moments. I had forgotten how they felt, and they feel good. I \
+            love adding things to my toolbox for later use."
+  },
+  {
+    id: "headingFive",
+    classSetting: "collapsed", //collapsed
+    control: "collapseFive",
+    week: "Week 5",
+    date: "September 25th 2022",
+    answer:"Not asking questions really hurt me. I need to remember that I there to learn, \
+            not to show people what I already know. Let my pride go and be comfortable being \
+            uncomfortable. I just thought the entire time we were making the mind reader,that \
+            there was a better easier way to do this. I would like to be able to code with more \
+            optimal solutions, and write better formatted code. I enjoy making people's life easier \
+            through applications and development. I also enjoy building things from scratch. This is \
+            something that I have been wanting to dive into the past 3 years or so. This is my \
+            chance to become a developer. "
+  },
+  {
+  id: "headingSix",
+  classSetting: "collapsed", //collapsed
+  control: "collapseSix",
+  week: "Week 6",
+  date: "October 2nd 2022",
+  answer:"Pseudocoding has been a huge help. I really have to get better at logically solving \
+          the problem before attempting. I struggle with figuring it out in my head and not waning to \
+          write it down. I have got to get better at pseudocoding. I also learned that it is very \
+          important to breakdown the problems into smaller problems. I really would like to expand \
+          more on named variables as well. I think that made my life so much easier. I could also \
+          see myself using them more in my projects. Makes code easy to read and easier to manage \
+          function parameters. What does it mean to be a good developer? Being able to meet the needs \
+          required in an appropriate amount of time. Also communication. Communicate what is going on in \
+          the project. Being a  good developer I would say is also just being a good person. Share \
+          victories with teamates and share burdens as well. Sharing solutions and lears is also a \
+          great way to be a good developer. I enjoy katas when I can figure them out. I would also say \
+          from experience, being rushed is something that as a developer I dont really like that much. \
+          I think it makes me a better developer though becuase I hit the goals that are needed and dont \
+          spend too much time on other things. I also think it minimizes scope creep. "
+},
 ];
