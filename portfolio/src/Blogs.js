2d0a148a1dffa1ab3ca2422458a18cef7f5e7363
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-10-31 20:21:08 +0000

    updated app.js

--- portfolio/src/Blogs.js
+++ portfolio/src/Blogs.js
@@ -1,33 +1,32 @@
-export const blogs =[
-    {
-        id:'headingOne',
-        classSetting:'', //collapsed
-        control: 'collapseOne',
-        date:'August 29th 2022',
-        question:'Was your first week what you expected? Why? Why not?',
-        question:'Was your first week what you expected? Why? Why not?',
-        answer:'Before the bootcamp, I really did not have any expectations,however I was not expecting the amount of reading we did this firstweek. I also did not expect to lear as much about git as I did! Theway that git works is so freaking cool. To know how the industryreleseas beta applications or features then deploys them to be the stable build is pretty great.'
-
-
-    },
-    {
-        id:'headingTwo',
-        classSetting:'collapsed', //collapsed
-        control: 'collapseTwo',
-        date:'September 3rd 2022',
-        question:'What is something you wish you could do but currently do not have the coding skills to accomplish?',
-        answer:'I wisht that I had more experience with the animations, transitions, and transform properties in css. I am not a fan of a overly designed page with so many animations and transitions. I view simplicity more beautiful than complication. The animations and transitions are sometimes a nice flavorful feature if done right. '
-
-
-    },
-    {
-        id:'headingThree',
-        classSetting:'collapsed', //collapsed
-        control: 'collapseThree',
-        date:'September 11th 2022',
-        question:'What are your thoughts about the roles HTML, CSS, and JavaScript play in the process of rendering content and providing user experience?',
-        answer:"HTML semantics are so fascinating to me. The HTML tag goes beyond just displaying to a page. They are also used by search engines and other web tools to be able to give the user a better experience. You could take the default styling off from the browser, and no matter what tag you use, it would all look the same. CSS is an absolute beast. You always think about languages that are difficult to pick up. CSS should be up there. With all of the different properties, there are some really cool things you can do. Technically, CSS is a style language but I see functionality support increasing over the past few years. Besides Just changing color and size of HTML elements, you are now able to calculate, create variables and more to be able to make apps more dynamic. This is needed because of how many different platforms we have now. including: web, ios, android, mac, windows and more I am sure. All of these platforms have their own different screen sizes and shapes. CSS has adapted to be able to help the developer create dynamic elements that can detect the platform it is on and adapt. I am trying to figure out how much time I want to spend on CSS vs Javascripts and other technologies. I mainly care more about App functionality and ease of use, however in my experience working with graphic designers and just as a consumer in general, It is  very important to make sure something not only looks good, but it's intuitive across all sorts of platforms. CSS, easy to learn, hard to get good at.  Javascript has been my favorite out of the technologies we have learned so far. I see it as a playground to be able to create solutions to an opportuniy. With JS, the sky is the limmit. Some people will express creativity in CSS and HTML; My creativity will be in Javascript. Thinking of logic to solve problems is my happy place. CSS and HTML are the things that people see where as JS is the 'Magic' that brings it together. It allows applications to interact with the world with APIs. I am so very thankful to have an oportunity to learn from trained professionals in this field."
-
-
-    },
+export const blogs = [
+  {
+    id: "headingOne",
+    classSetting: "", //collapsed
+    control: "collapseOne",
+    date: "August 29th 2022",
+    question: "Was your first week what you expected? Why? Why not?",
+    question: "Was your first week what you expected? Why? Why not?",
+    answer:
+      "Before the bootcamp, I really did not have any expectations,however I was not expecting the amount of reading we did this firstweek. I also did not expect to lear as much about git as I did! Theway that git works is so freaking cool. To know how the industryreleseas beta applications or features then deploys them to be the stable build is pretty great.",
+  },
+  {
+    id: "headingTwo",
+    classSetting: "collapsed", //collapsed
+    control: "collapseTwo",
+    date: "September 3rd 2022",
+    question:
+      "What is something you wish you could do but currently do not have the coding skills to accomplish?",
+    answer:
+      "I wisht that I had more experience with the animations, transitions, and transform properties in css. I am not a fan of a overly designed page with so many animations and transitions. I view simplicity more beautiful than complication. The animations and transitions are sometimes a nice flavorful feature if done right. ",
+  },
+  {
+    id: "headingThree",
+    classSetting: "collapsed", //collapsed
+    control: "collapseThree",
+    date: "September 11th 2022",
+    question:
+      "What are your thoughts about the roles HTML, CSS, and JavaScript play in the process of rendering content and providing user experience?",
+    answer:
+      "HTML semantics are so fascinating to me. The HTML tag goes beyond just displaying to a page. They are also used by search engines and other web tools to be able to give the user a better experience. You could take the default styling off from the browser, and no matter what tag you use, it would all look the same. CSS is an absolute beast. You always think about languages that are difficult to pick up. CSS should be up there. With all of the different properties, there are some really cool things you can do. Technically, CSS is a style language but I see functionality support increasing over the past few years. Besides Just changing color and size of HTML elements, you are now able to calculate, create variables and more to be able to make apps more dynamic. This is needed because of how many different platforms we have now. including: web, ios, android, mac, windows and more I am sure. All of these platforms have their own different screen sizes and shapes. CSS has adapted to be able to help the developer create dynamic elements that can detect the platform it is on and adapt. I am trying to figure out how much time I want to spend on CSS vs Javascripts and other technologies. I mainly care more about App functionality and ease of use, however in my experience working with graphic designers and just as a consumer in general, It is  very important to make sure something not only looks good, but it's intuitive across all sorts of platforms. CSS, easy to learn, hard to get good at.  Javascript has been my favorite out of the technologies we have learned so far. I see it as a playground to be able to create solutions to an opportuniy. With JS, the sky is the limmit. Some people will express creativity in CSS and HTML; My creativity will be in Javascript. Thinking of logic to solve problems is my happy place. CSS and HTML are the things that people see where as JS is the 'Magic' that brings it together. It allows applications to interact with the world with APIs. I am so very thankful to have an oportunity to learn from trained professionals in this field.",
+  },
 ];
