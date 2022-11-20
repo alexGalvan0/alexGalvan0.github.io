0d0a971b9fed1da28fc6009f041a34bb7ba12a98
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-11-20 13:48:10 +0000

    update 1

---
+++ next-portfolio/pages/about.jsx
@@ -0,0 +1,12 @@
+import Nav from "./components/Nav";
+
+function About(){
+    return(
+        <>
+        <Nav />
+        <h1>About Page!</h1>
+        </>
+    )
+}
+
+export default About
---
+++ next-portfolio/pages/index.jsx
@@ -0,0 +1,17 @@
+import styles from '../styles/Home.module.css'
+import Nav from './components/Nav'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className="container-fluid">
+      <Nav />
+        <div className="row">
+          <div className="col">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
